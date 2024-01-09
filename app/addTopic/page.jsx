c86dc3077b3895c014d55a8f1044cf51ad59015d
theadamites <adamelgendy1234@gmail.com>
2024-01-08 20:41:42 -0500

    Next.js
    Next.js CRUD

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description are required.");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description, location }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Resource Title"
+      />
+
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Resource Description"
+      />
+      <input
+        onChange={(e) => setLocation(e.target.value)}
+        value={location}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Resource Location"
+      />
+
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Add Topic
+      </button>
+    </form>
+  );
+}
